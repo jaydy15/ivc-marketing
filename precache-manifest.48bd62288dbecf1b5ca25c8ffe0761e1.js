31069f8c920b59ccfa987ae8e28efa2e8f9c0f34
jay <dyjay15@gmail.com>
2020-09-21 09:55:05 +0800

    Updates

---
+++ precache-manifest.48bd62288dbecf1b5ca25c8ffe0761e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c009a58ef7d5fd358d4c009e37a9dfa",
+    "url": "/ivc-marketing/index.html"
+  },
+  {
+    "revision": "c513ecf73acac90b93c0",
+    "url": "/ivc-marketing/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fb72ae5ceb7b636a8482",
+    "url": "/ivc-marketing/static/css/main.dd193482.chunk.css"
+  },
+  {
+    "revision": "c513ecf73acac90b93c0",
+    "url": "/ivc-marketing/static/js/2.3807911d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ivc-marketing/static/js/2.3807911d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb72ae5ceb7b636a8482",
+    "url": "/ivc-marketing/static/js/main.7912b3f2.chunk.js"
+  },
+  {
+    "revision": "0bbba38ff325615f2586",
+    "url": "/ivc-marketing/static/js/runtime-main.18cdb231.js"
+  }
+]);
